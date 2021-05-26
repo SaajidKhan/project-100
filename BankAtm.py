bc3cccbc2289398f85c0fa7b5173caf64dc46340
SaajidKhan <69465011+SaajidKhan@users.noreply.github.com>
2021-05-26 17:29:04 +0530

    Add files via upload

---
+++ BankAtm.py
@@ -0,0 +1,15 @@
+class Atm(object):
+    def __init__(self,atm_card_number,pin_number):
+        self.atm_card_number=atm_card_number
+        self.pin_number=pin_number
+
+        def CashWithdrawl(self):
+            print("Congratulaions!! Your cash has been withdrawn!!")
+
+        def BalanceEnquiry(self):
+            print("Balance has been updated!!")
+
+Axis=Atm(567820,641663)
+print(Axis.atm_card_number)
+print(Axis.pin_number)
+
